aa8f6e606ee29dc461895bb085af945bb0e50ac8
Jesher Joshua <jesherjoshua@Jeshers-MacBook-Air.local>
2021-11-05 18:12:00 +0530

    added username obj and created usernames txt file

--- extractor.py
+++ extractor.py
@@ -3,10 +3,12 @@ import re
 
 unames=[]
 co=open('/Users/jesherjoshua/Documents/jesher/Programming/Python/secrets/_chat.txt')
+uo=open('/Users/jesherjoshua/Documents/jesher/Programming/Python/secrets/usernames.txt','w')
 ig_uname_regex =re.compile(r'@(?:(?:[\w][\.]{0,1})*[\w]){1,30}')
 for i in co:
     if len(ig_uname_regex.findall(i))==0 or ig_uname_regex.findall(i)[0][1:].isdigit():continue
     else:
         unames.append(ig_uname_regex.findall(i)[0])
 unames_str='\n'.join(unames)
-print(unames_str)
+uo.write(unames_str)
+uo.close()
